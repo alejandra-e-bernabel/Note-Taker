297a92ea8086c7edac56580e8595e52192293537
Alejandra Camacho <alejandra.e.bernabel@gmail.com>
2023-10-01 23:37:56 -0400

    Added duuid npm package

--- server.js
+++ server.js
@@ -25,5 +25,12 @@ app.get("*", (req, res) => {
 });
 
 
+// * `POST /api/notes` should receive a new note to save on the request body, 
+//add it to the `db.json` file, and then return the new note to the client. 
+//You'll need to find a way to give each note a unique id when it's saved 
+//(look into npm packages that could do this for you).
+
+
+
 app.listen(PORT, () => 
     console.log("App listening at http://localhost:" + PORT));
