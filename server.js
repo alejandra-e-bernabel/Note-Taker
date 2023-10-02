694d9e8d2fa21955f3b400f5b72c66ec6ec5e697
Alejandra Camacho <alejandra.e.bernabel@gmail.com>
2023-10-01 23:35:21 -0400

    Created paths for getting to notes and index pages, as well as a get path for the db json api

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require ("express");
 const path = require("path");
 
+const noteData = require ("./db/db.json");
+
 const PORT = 3001;
 
 const app = express();
@@ -10,7 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.get("/")
+app.get("/notes", (req, res) => 
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+app.get("/api/notes", (req, res) => {
+    res.json(noteData);
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+});
 
 
 app.listen(PORT, () => 
